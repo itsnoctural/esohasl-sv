a64833c5c45016d643cad9f82e0be7727848a080
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-06-01 18:54:31 +0000

    style: svelte/no-useless-mustaches

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
 		class="bg-background/75 mx-auto flex w-full max-w-screen-xl items-center justify-center rounded-xl border border-t p-3"
 	>
 		<span>&copy; esohasl.net | Built with ❤️ by&nbsp;</span>
-		<a href={'https://www.youtube.com/@esohasl'} target="_blank" class="text-muted-foreground">
+		<a href="https://www.youtube.com/@esohasl" target="_blank" class="text-muted-foreground">
 			esohasl
 		</a>
 	</div>
