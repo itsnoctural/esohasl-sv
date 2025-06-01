c132f989a9bd7da2cd49d16b41ef6f74a912dffe
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-06-01 18:58:06 +0000

    feat: @vercel/speed-insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+injectSpeedInsights({ sampleRate: 0.8 });
 
 export const prerender = true;
