2c01bcca0c11be2df93f30786f76c1bfaf1c9474
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-06-20 11:57:44 +0000

    chore: remove sampleRate on speed-insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,6 @@ import { injectAnalytics } from '@vercel/analytics/sveltekit';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
-injectSpeedInsights({ sampleRate: 0.8 });
+injectSpeedInsights();
 
 export const prerender = true;
